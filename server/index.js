fd952d80fa419ee81488c2b160c503396600d635
AlexanderZhao <zhaomengzhe@qq.com>
2018-03-14 23:21:39 +0800

    add test page

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
+
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile('./page.html');
 });
 
 var server = app.listen(3000, function () {
