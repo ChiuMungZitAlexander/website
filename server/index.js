c10fefbabc1741369b64303a49a91e5f4ff426d5
AlexanderZhao <zhaomengzhe@qq.com>
2018-03-26 18:35:48 +0800

    use express router

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const path = require('path')
 const express = require('express');
 const http = require('http')
+
 const app = express();
+const router = express.Router()
 
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 
-app.get('/', function (req, res) {
-	res.sendFile(path.resolve('client/view/index.html'));
+router.use(function timeLog (req, res, next) {
+	console.log('Time: ', Date.now());
+	next();
 });
+
+router.get('', function (req, res) {
+	res.redirect('/index')
+});
+
+router.get('index', function (req, res) {
+	res.sendFile(path.resolve('client/view/index.html'));
+})
 
 const server = http.createServer(app).listen(80, 'localhost', function () {
 	const host = server.address().address;
