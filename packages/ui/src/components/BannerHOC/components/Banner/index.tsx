cf356d2bd8b9f8f6c7e9f823cf7e599b4a9d9cb5
AlexanderZhao <zhaomengzhe@qq.com>
2019-06-11 01:03:21 +0800

    add banner styles

---
+++ packages/ui/src/components/BannerHOC/components/Banner/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+export const Banner = () => <div className="banner-container">
+  <ul className="banner-item-list">
+    <li className="banner-item personal-icon" />
+    <li className="banner-item">
+      menu1
+    </li>
+    <li className="banner-item">
+      menu2
+    </li>
+    <li className="banner-item">
+      menu3
+    </li>
+    <li className="banner-item">
+      menu4
+    </li>
+  </ul>
+</div>
--- packages/ui/src/components/BannerHOC/index.tsx
+++ packages/ui/src/components/BannerHOC/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
+import { Banner } from './components/Banner/'
+
 export const BannerHOC = (WrappedComp: React.ComponentType) => () => <div className="wrapped-banner-container">
-  <div>banner</div>
+  <Banner />
   <WrappedComp />
 </div>
