aca729e15e2f5361bfa02c2e584918004318d53d
AlexanderZhao <zhaomengzhe@qq.com>
2019-06-10 19:57:06 +0800

    refactor with typescript

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(express.static(`${__dirname}/../dist`));
+app.use(express.static(`${__dirname}/../dist/`));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../dist/index.html`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,34 @@
-const path = require('path');
-const webpack = require('webpack');
-
-const SRC_DIR = path.join(__dirname, '/src');
-const DIST_DIR = path.join(__dirname, '/dist');
-
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: "./src/index.tsx",
   output: {
-    path: DIST_DIR,
-    filename: 'bundle.js',
+    filename: "bundle.js",
+    path: __dirname + "/dist"
   },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.css']
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
   },
+
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.png$/,
-        loader: 'url-loader?limit=100000&minetype=image/png'
-      },
-      {
-        test: /\.jpg/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-react', '@babel/preset-env']
-        }
-      }
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
-};
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+}
