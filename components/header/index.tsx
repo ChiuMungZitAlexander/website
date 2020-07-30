1880841c0290862364470c0490b8d2622ffcf37e
AlexanderZhao <zhaomengzhe@qq.com>
2020-07-30 21:56:13 +0800

    chore/add-prettierrc

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link"
+import Link from 'next/link'
 
-import headerStyle from "../../styles/components/header.module.scss"
+import headerStyle from '../../styles/components/header.module.scss'
 
 const Header = (): JSX.Element => (
   <nav className={headerStyle.header}>
