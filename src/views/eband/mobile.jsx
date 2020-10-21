7fda6cb14653d48e5d9089382ce91eca019fa950
AlexanderZhao <zhaomengzhe@qq.com>
2020-10-22 02:43:01 +0800

    update/update-layout

--- src/views/eband/mobile.jsx
+++ src/views/eband/mobile.jsx
@@ -23,8 +23,22 @@ const EbandMobile = () => (
         <div class="button-down"></div>
       </p>
     </div>
-    <div className="eband mobile">
+    <div className="eband" style={{ height: '50%' }}>
       <div className="eband-img" />
+    </div>
+    <div className="eband mobile">
+      <p className="eband-member-row instrument">萨克斯</p>
+      <p className="eband-member-row">赵梦哲</p>
+      <p className="eband-member-row instrument">非洲鼓</p>
+      <p className="eband-member-row">尼尔·麦克米兰(苏格兰)</p>
+      <p className="eband-member-row instrument">节奏吉他/巴拉莱卡</p>
+      <p className="eband-member-row">迪密特里·安东诺夫(俄罗斯)</p>
+      <p className="eband-member-row instrument">主音吉他/贝司</p>
+      <p className="eband-member-row">尼克·特鲁诺夫(乌克兰)</p>
+      <p className="eband-member-row instrument">旋律吉他</p>
+      <p className="eband-member-row">桑迪普·克拉姆贝(印度)</p>
+      <p className="eband-member-row instrument">键盘</p>
+      <p className="eband-member-row">向婕</p>
     </div>
   </>
 )
