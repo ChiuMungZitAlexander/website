4d3df1ea13914a890c70a560bd62b2091bc22c38
AlexanderZhao <zhaomengzhe@qq.com>
2021-01-30 10:31:22 +0800

    feat/create-redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,12 @@
 const path = require('path')
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
+  const { createPage, createRedirect } = actions
+
+  createRedirect({
+    fromPath: '/',
+    toPath: '/zh',
+  })
 
   const result = await graphql(`
     {
