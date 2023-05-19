98ac5fea931e1d7a1b9831d9f4363155908b3a2b
AlexanderZhao <zhaomengzhe@gmail.com>
2023-05-19 19:24:56 +0800

    chore(): config alias

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,10 +1,22 @@
-import type { CreateBabelConfigArgs } from 'gatsby'
+import path from 'path'
+
+import type { CreateBabelConfigArgs, CreateWebpackConfigArgs } from 'gatsby'
 
 exports.onCreateBabelConfig = ({ actions }: CreateBabelConfigArgs) => {
   actions.setBabelPlugin({
     name: '@babel/plugin-transform-react-jsx',
     options: {
       runtime: 'automatic',
+    },
+  })
+}
+
+exports.onCreateWebpackConfig = ({ actions }: CreateWebpackConfigArgs) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
   })
 }
