df200f9ba8fbf49c756061bca76e7e7f439ce707
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-14 20:00:50 +0800

    feat(): split en/zh translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,34 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import en from "./locale/en/index.json";
+import zh from "./locale/zh/index.json";
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-  en: {
-    translation: {
-      about: "about",
-      blogs: "blogs",
-      music: "music",
-      more: "more",
-      intro_1: "I'm Alexander ZHÀO",
-      intro_2: "Web3 developer (Typescript/Rust)",
-      intro_3: "Alto saxophinst",
-      coming_soon: "coming soon",
-    },
-  },
-  zh: {
-    translation: {
-      about: "关于",
-      blogs: "博客",
-      music: "音乐",
-      more: "更多",
-      intro_1: "我是 Alexander ZHÀO",
-      intro_2: "Web3开发者 (Typescript/Rust)",
-      intro_3: "中音萨克斯手",
-      coming_soon: "即将上线",
-    },
-  },
+  en,
+  zh,
 };
 
 i18n
