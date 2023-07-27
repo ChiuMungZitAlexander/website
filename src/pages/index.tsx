e8926a3af26104770884168a992dc7da5d46d635
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-27 20:09:59 +0800

    feat(): add scroll down tip icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,17 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import { GatsbyImage, IGatsbyImageData, getImage } from "gatsby-plugin-image";
-import { Box, Flex, Text, Anchor } from "@mantine/core";
+import {
+  Box,
+  Flex,
+  Text,
+  Anchor,
+  rem,
+  createStyles,
+  keyframes,
+} from "@mantine/core";
+import { useWindowScroll } from "@mantine/hooks";
+import { IconArrowBigDownLine } from "@tabler/icons-react";
 
 import Layout from "@/layouts";
 
@@ -12,13 +22,64 @@ type IndexPageProps = {
   };
 };
 
+const bounce = keyframes({
+  "from, 20%, 53%, 80%, to": { transform: "translate3d(0, 0, 0)" },
+  "40%, 43%": { transform: "translate3d(0,-10px, 0)" },
+  "70%": { transform: "translate3d(0, -8px, 0)" },
+  "90%": { transform: "translate3d(0, -2px, 0)" },
+});
+
+const useStyles = createStyles(() => ({
+  icon: {
+    animation: `${bounce} 2s ease-in-out infinite`,
+  },
+}));
+
 const IndexPage = ({ data }: IndexPageProps) => {
   const { t } = useTranslation();
 
+  const [{ y }] = useWindowScroll();
+  const { classes } = useStyles();
+
+  const [isMobileDevice, setIsMobileDevice] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMobileDevice(/Mobi/i.test(navigator.userAgent));
+  }, []);
+
   return (
-    <Layout>
+    <Layout headerStyles={{ backgroundColor: "transparent" }}>
       <>
-        <Flex h="calc(100vh - 72px - 24px)"></Flex>
+        <Box h="100vh" mt={rem(-(72 + 24))} mx={rem(-24)}>
+          <video
+            autoPlay
+            controls={isMobileDevice ? true : false}
+            height="80%"
+            loop
+            muted
+            playsInline
+            poster="https://picsum.photos/1200"
+            style={{
+              left: 0,
+              objectFit: "cover",
+              position: "absolute",
+              right: 0,
+              top: 0,
+            }}
+            width="100%"
+          >
+            <source
+              src="https://samplelib.com/lib/preview/mp4/sample-5s.mp4"
+              type="video/mp4"
+            />
+          </video>
+          <Flex align="center" direction="column-reverse" h="100%">
+            <IconArrowBigDownLine
+              className={classes.icon}
+              display={y > 120 ? "none" : "block"}
+            />
+          </Flex>
+        </Box>
         <Flex align="center" direction="column" h="100vh" justify="center">
           <GatsbyImage
             alt="avatar"
@@ -109,11 +170,7 @@ export default IndexPage;
 export const query = graphql`
   query ($language: String!) {
     imageSharp(fixed: { originalName: { eq: "avatar.jpg" } }) {
-      gatsbyImageData(
-        placeholder: BLURRED
-        width: 128
-        height: 128
-      )
+      gatsbyImageData(placeholder: BLURRED, width: 128, height: 128)
     }
     locales: allLocale(filter: { language: { eq: $language } }) {
       edges {
