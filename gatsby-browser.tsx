41adc1c18ef9d843b14c95e94055dd002a0ef2b1
AlexanderZhao <zhaomengzhe@gmail.com>
2023-07-27 12:52:02 +0800

    feat(): add react query

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { MantineUIProvider } from "./src/contexts/mantine";
 
@@ -6,8 +7,12 @@ import type { WrapRootElementBrowserArgs } from "gatsby";
 
 import "./global.css";
 
+const queryClient = new QueryClient();
+
 export const wrapRootElement = ({ element }: WrapRootElementBrowserArgs) => (
-  <MantineUIProvider>{element}</MantineUIProvider>
+  <MantineUIProvider>
+    <QueryClientProvider client={queryClient}>{element}</QueryClientProvider>
+  </MantineUIProvider>
 );
 
 export const onRouteUpdate = () => {
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { MantineUIProvider } from "./src/contexts/mantine";
 
 import type { WrapRootElementNodeArgs, RenderBodyArgs } from "gatsby";
 
+const queryClient = new QueryClient();
+
 export const wrapRootElement = ({ element }: WrapRootElementNodeArgs) => (
-  <MantineUIProvider>{element}</MantineUIProvider>
+  <MantineUIProvider>
+    <QueryClientProvider client={queryClient}>{element}</QueryClientProvider>
+  </MantineUIProvider>
 );
 
 export const onRenderBody = ({
